bf942edd8a47b47c78945a9f90d5e3b8de7dc6ba
halixiedun <1158412582@qq.com>
2025-06-26 22:40:13 +0800

    ci3

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,11 @@
 # tests/test_app.py
+import sys
+import os
 import unittest
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import greet
 
 class TestGreet(unittest.TestCase):
     def test_greet(self):
         self.assertEqual(greet("World"), "Hello, World!")
-
-if __name__ == "__main__":
-    unittest.main()
